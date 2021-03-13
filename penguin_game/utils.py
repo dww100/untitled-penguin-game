ec3ae4a0f16e933cdd1e4ce8e853fb1b2a591e6f
Vytautas Jancauskas <unaudio@gmail.com>
2021-03-13 17:13:03 +0100

    play electric

--- penguin_game/utils.py
+++ penguin_game/utils.py
@@ -1,7 +1,8 @@
 import pygame as pg
 
 def play_sound(sound):
-    pg.mixer.Channel(sound[1]).stop()
+    if pg.mixer.Channel(sound[1]).get_busy():
+        pg.mixer.Channel(sound[1]).stop()
     pg.mixer.Channel(sound[1]).play(sound[0])
 
 class SpriteSheet:
